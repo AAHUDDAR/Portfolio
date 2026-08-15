90a21f089f5782afcac482e6abec0e298bc8a0e8
Aditya Huddar <aditya.huddar3@gmail.com>
2026-08-15 15:00:19 +0530

    Fix mobile responsive design

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ navbar.classList.remove("scrolled");
 }
 
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
